93958a079e4d9d1ce6740e3c88166e0f8ba65ae4
gnavadev <navag@oregonstate.edu>
2026-06-10 13:21:12 -0700

    aws credentials

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.10"
 
   backend "s3" {
-    bucket       = "REPLACE-WITH-YOUR-UNIQUE-BUCKET-NAME" # from scripts/00-bootstrap-state.sh
+    bucket       = "navag-minecraft-project"
     key          = "minecraft/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = true # S3-native locking, no DynamoDB needed
+    use_lockfile = true
   }
 }
